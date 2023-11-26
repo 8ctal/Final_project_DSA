99e7fce33423b7c1621515a40a372ea7041ae6e3
Wissbegierde <juandavidmenag@gmail.com>
2023-11-25 21:58:45 -0500

    Estructura

--- grafo/Grafo.java
+++ grafo/Grafo.java
@@ -12,17 +12,9 @@ public class Grafo {
         this.matrizAdyacencia = new int[nodos][nodos];
     }
     
-<<<<<<< HEAD
     public void addArista(int u, int v, int peso)
     {
         matrizAdyacencia[u][v] = peso;
-=======
-    public void addArista(int u, int v, Nodo nodo)
-    {
-        matrizAdyacencia[u][v] = 0;
-        
-
->>>>>>> 86c9544663138c8e723c89b469a333a8de587ecf
         A++;
     }
 }
